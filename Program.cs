f3ea262a301791977be49a2baa93f471df93e90a
George El-jaji <george.el-jaji@exopen.se>
2017-04-02 15:10:44 +0200

    refactoring.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ namespace Fibbonacci
         static void Main(string[] args)
         {
             // Sequence 
-            WriteOutFinbonacciSeries(length: 10);
+            WriteOutFibonacciSequence(length: 10);
 
             Console.WriteLine();
 
             // Specific number
-            var number =  GetFibonacciNumber(index: 7);
+            var number = GetFibonacciNumber(index: 7);
             Console.WriteLine(number);
         }
 
         static int GetFibonacciNumber(int index)
         {
-            var n2 = 0;
-            var n1 = 1;
+            int n2 = 0, // f(n-2)
+            n1 = 1; // f(n-1)
 
             if (index == 0) return n2;
             if (index == 1) return n1;
@@ -31,7 +31,7 @@ namespace Fibbonacci
 
             for (var i = 2; i <= index; i++)
             {
-                var n = n1 + n2;
+                var n = n1 + n2; // f(n) = f(n-1) + f(n-2)
                 sequence[i] = n;
 
                 n2 = n1;
@@ -41,16 +41,17 @@ namespace Fibbonacci
             return sequence[index];
         }
 
-        static void WriteOutFinbonacciSeries(int length)
+        static void WriteOutFibonacciSequence(int length)
         {
-            Console.Write("0 ");
-            Console.Write("1 ");
+            if (length >= 0) Console.Write("0 ");
+            if (length >= 1) Console.Write("1 ");
 
-            int n2 = 0, n1 = 1;
+            int n2 = 0, // f(n-2)
+                n1 = 1; // f(n-1)
 
             for (var i = 2; i < length; i++)
             {
-                var n = n1 + n2;
+                var n = n1 + n2; // f(n) = f(n-1) + f(n-2)
                 Console.Write($"{n} ");
 
                 n2 = n1;
